c7e667d61d501c3f6827eb42160f5f04a20fd978
Molochko1990 <es01@bk.ru>
2024-06-24 13:20:28 +0500

    Убрал ошибку с импортом

--- game/mvc/model/game_state.py
+++ game/mvc/model/game_state.py
@@ -3,7 +3,7 @@ import random
 
 from ...assets_loader import UNIT_IMAGES
 from ...ecs.entities.entities import CityEntity, SoldierEntity, TankEntity
-from ...ecs.entity_creator import create_soldier, create_city, create_entity
+from ...ecs.entity_creator import create_entity
 from ...settings import *
 
 
